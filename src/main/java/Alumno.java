7bc84c61e98951c6b294e748fc7f183dcf23381f
csalinas10 <c.salinas10@ufromail.cl>
2022-10-01 17:15:07 -0300

    Avance 1

---
+++ src/main/java/Alumno.java
@@ -0,0 +1,42 @@
+public class Alumno {
+
+    private String nombre, apellido, rut;
+    private int numeroMatricula;
+
+    public Alumno ( int numeroMatricula , String nombre, String apellido, String rut) {
+        this.numeroMatricula = numeroMatricula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.rut = rut;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
+    public int getNumeroMatricula() {
+        return numeroMatricula;
+    }
+    public void setNumeroMatricula(int numeroMatricula) {
+        this.numeroMatricula = numeroMatricula;
+    }
+}
+

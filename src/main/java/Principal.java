d7ea5f6e607e671034330681cbca12021dad2b88
csalinas10 <c.salinas10@ufromail.cl>
2022-10-02 20:52:32 -0300

    Avance finali

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -1,16 +1,17 @@
 
 public class Principal {
-    public Alumno alu (int numeroMatricula, String nombre, String apellido, String rut) {
+    public Alumno alu(int numeroMatricula, String nombre, String apellido, String rut) {
         Alumno alumnoNuevo = new Alumno(numeroMatricula, nombre, apellido, rut);
         Curso curso = new Curso();
-        curso.agregarAlumno(numeroMatricula,nombre,apellido,rut);
-        curso.modificarCaracteristicasAlumno(nombre,apellido);
+        curso.agregarAlumno(numeroMatricula, nombre, apellido, rut);
+        curso.modificarCaracteristicasAlumno(nombre, apellido);
         curso.eliminarAlumno(alumnoNuevo);
-        curso.moverAlumno(nombre,apellido);
+        curso.moverAlumno(nombre, apellido);
         ValidarRut validarRut = new ValidarRut();
         return alumnoNuevo;
     }
+
     public static void main(String[] args) {
         new Principal();
-        private ValidarRut
+    }
 }
